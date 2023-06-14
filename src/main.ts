35deba1d35e17a53f9d0a1eae22aeac6f8d1d64c
Brandon Jordan <brandonjordan124@gmail.com>
2023-06-13 22:21:13 -0400

    Add ability to parse plist data

--- src/main.ts
+++ src/main.ts
@@ -5,33 +5,50 @@ import './style.css';
 
 import {renderShortcut} from "~/render";
 
+// @ts-ignore
+import {parse} from 'plist/dist/plist-parse.js';
+
 export let preview: HTMLDivElement | null;
 
-export function previewShortcut(shortcut: string | object, selector: string = '#shortcut-preview') {
-    console.group('[preview-shortcut]');
+export function previewShortcutURL(url: string, selector: string = '#shortcut-preview') {
+    fetch(url).then(response => response.text()).then((response) => {
+        let json;
+        try {
+            json = JSON.parse(response);
+        } catch (e) {
+            json = parse(response);
+        }
+        previewShortcut(json, selector);
+    }).catch(error => {
+        console.error(`[preview-shortcut] Error loading shortcut: ${error}`);
+    });
+}
 
+export function previewShortcut(shortcut: string | object, selector: string = '#shortcut-preview') {
     preview = document.querySelector<HTMLDivElement>(selector);
     if (!preview) {
         throw new Error(`[preview-shortcut] Selector '${selector}' selects nothing.`);
     }
-    preview.className = 'sp-container ios';
+    if (!preview.className.includes('sp-container')) {
+        let previewClasses = preview.className.split(' ');
+        previewClasses.push('sp-container', 'ios');
+        preview.className += previewClasses.join(' ');
+    }
     preview.innerHTML = '';
 
-    const shortcutDataType = typeof shortcut;
-    switch (shortcutDataType) {
+    switch (typeof shortcut) {
         case 'string':
-            fetch(String(shortcut)).then(response => response.json()).then((response) => {
-                renderShortcut(response.WFWorkflowActions);
-                console.groupEnd();
-            }).catch(error => {
-                console.error(`[preview-shortcut] Error loading shortcut: ${error}`);
-            });
+            let json;
+            try {
+                json = JSON.parse(shortcut);
+            } catch (e) {
+                json = parse(shortcut);
+            }
+            renderShortcut(json.WFWorkflowActions);
             break;
         case 'object':
-            renderShortcut(Object(shortcut));
-            console.groupEnd();
+            // @ts-ignore
+            renderShortcut(shortcut.WFWorkflowActions);
             break;
-        default:
-            throw new Error(`[preview-shortcut] Shortcut data is not of type URL string or object but ${shortcutDataType}!`);
     }
 }
