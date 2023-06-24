3d47749b867dcc9cac165728b793a3b23fe192c5
Brandon Jordan <brandonjordan124@gmail.com>
2023-06-24 16:34:52 -0400

    Implement more of IfElse action

--- src/actions/if-else.ts
+++ src/actions/if-else.ts
@@ -3,7 +3,9 @@ import {renderActionIcon, renderListItem} from "~/render";
 
 interface IfElseParameters {
     WFInput: string | object,
+    WFConditionalActionString: string | object
     WFControlFlowMode: number,
+    WFCondition: number,
 }
 
 export default {
@@ -37,6 +39,62 @@ export default {
             input.style.display = 'inline-block';
             input.innerHTML = renderValue(params['WFInput'] ?? null, 'Items');
             action.appendChild(input);
+
+            //WFCondition
+            let conditionType;
+            switch (params['WFCondition']) {
+                case 4:
+                    conditionType = 'is';
+                    break;
+                case 5:
+                    conditionType = 'is not';
+                    break;
+                case 100:
+                    conditionType = 'has any value';
+                    break;
+                case 101:
+                    conditionType = 'does not have any value';
+                    break;
+                case 99:
+                    conditionType = 'contains';
+                    break;
+                case 999:
+                    conditionType = 'does not contain';
+                    break;
+                case 8:
+                    conditionType = 'begins with';
+                    break;
+                case 9:
+                    conditionType = 'ends with';
+                    break;
+                case 2:
+                    conditionType = 'greater than';
+                    break;
+                case 3:
+                    conditionType = 'greater or equal to';
+                    break;
+                case 0:
+                    conditionType = 'less than';
+                    break;
+                case 1:
+                    conditionType = 'less or equal to';
+                    break;
+                case 1003:
+                    conditionType = 'between';
+                    break;
+            }
+            const condition = document.createElement('div');
+            condition.style.display = 'inline-block';
+            condition.className = 'sp-value';
+            condition.innerText = String(conditionType);
+            action.appendChild(condition);
+
+            if (params['WFConditionalActionString']) {
+                const value = document.createElement('div');
+                value.style.display = 'inline-block';
+                value.innerHTML = renderValue(params['WFConditionalActionString'] ?? null, 'Text');
+                action.appendChild(value);
+            }
         }
 
         return renderListItem(renderActionIcon('arrow_branch', 'white', '#8e8e93'), action.outerHTML);
