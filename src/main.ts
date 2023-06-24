19d0c098d9574fd54c6bd34e6a1830aa04698289
Brandon Jordan <brandonjordan124@gmail.com>
2023-06-24 17:22:03 -0400

    Add Shortcut info header

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,25 @@ import Framework7 from "framework7";
 
 export let preview: HTMLDivElement | null;
 
-export function previewShortcutURL(url: string, selector: string = '#shortcut-preview') {
+const colors = {
+    "4282601983": "#f36269",
+    "4251333119": "#fc8567",
+    "4271458815": "#f7a752",
+    "4274264319": "#edc400",
+    "4292093695": "#42c95f",
+    "431817727": "#2dcdad",
+    "1440408063": "#2fcbde",
+    "463140863": "#2cb4f7",
+    "946986751": "#4969c7",
+    "2071128575": "#8757c3",
+    "3679049983": "#bb7ae5",
+    "3980825855": "#f68bd4",
+    "3031607807": "#9e908d",
+    "2846468607": "#939f96",
+    "255": "#86929a",
+}
+
+export function previewShortcutURL(url: string, selector: string = '#shortcut-preview', name?: string) {
     fetch(url).then(response => {
         if (response.status !== 200) {
             throw new Error(`Unable to load shortcut (${response.status}): ${url}`);
@@ -24,13 +42,13 @@ export function previewShortcutURL(url: string, selector: string = '#shortcut-pr
         } catch (e) {
             json = parse(response);
         }
-        previewShortcut(json, selector);
+        previewShortcut(json, selector, name);
     }).catch(error => {
         console.error(`[preview-shortcut] ${error}`);
     });
 }
 
-export function previewShortcut(shortcut: string | object, selector: string = '#shortcut-preview') {
+export function previewShortcut(shortcut: string | object, selector: string = '#shortcut-preview', name?: string) {
     preview = document.querySelector<HTMLDivElement>(selector);
     if (!preview) {
         throw new Error(`[preview-shortcut] Selector '${selector}' selects nothing.`);
@@ -46,19 +64,39 @@ export function previewShortcut(shortcut: string | object, selector: string = '#
     }
     preview.innerHTML = '';
 
-    switch (typeof shortcut) {
-        case 'string':
-            let json;
-            try {
-                json = JSON.parse(shortcut);
-            } catch (e) {
-                json = parse(shortcut);
-            }
-            renderShortcut(json.WFWorkflowActions);
-            break;
-        case 'object':
-            // @ts-ignore
-            renderShortcut(shortcut.WFWorkflowActions);
-            break;
+    let data;
+    if (typeof shortcut === 'string') {
+        try {
+            data = JSON.parse(shortcut);
+        } catch (e) {
+            data = parse(shortcut);
+        }
+    } else {
+        data = shortcut;
     }
+
+    const header = document.createElement('div');
+    header.className = 'sp-header';
+
+    const iconContainer = document.createElement('div');
+    iconContainer.className = 'sp-info';
+
+    const icon = document.createElement('div');
+    icon.className = 'sp-icon';
+    if (data.WFWorkflowIcon) {
+        // @ts-ignore
+        icon.style.backgroundColor = colors[data.WFWorkflowIcon.WFWorkflowIconStartColor];
+    }
+    iconContainer.appendChild(icon);
+
+    const title = document.createElement('div');
+    title.className = 'sp-header-title';
+    title.innerText = name ?? data.WFWorkflowName ?? 'Shortcut';
+    iconContainer.appendChild(title);
+
+    header.appendChild(iconContainer);
+
+    preview.appendChild(header);
+
+    renderShortcut(data.WFWorkflowActions);
 }
