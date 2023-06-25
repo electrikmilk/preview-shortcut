632f26139ebb83875f8493917dab2cb645a55142
Brandon Jordan <brandonjordan124@gmail.com>
2023-06-24 23:07:49 -0400

    Add dark mode support to Nothing action

--- src/actions/nothing.ts
+++ src/actions/nothing.ts
@@ -1,6 +1,19 @@
+import {renderActionIcon, renderListItem} from "~/render";
+
 export default {
-    title: 'Nothing',
-    background: 'transparent',
-    color: 'black',
-    icon: 'viewfinder'
+    render: (container: HTMLElement) => {
+        container.className += ' sp-nothing-action';
+
+        const action = document.createElement('div');
+        action.style.display = 'flex';
+        action.style.justifyItems = 'inline-flex';
+
+        action.style.gap = '0 10px';
+        const header = document.createElement('div');
+        header.className = 'sp-action-title';
+        header.innerText = 'Nothing';
+
+        action.appendChild(header);
+        return renderListItem(renderActionIcon('viewfinder', 'black', 'transparent'), action.outerHTML);
+    }
 }
