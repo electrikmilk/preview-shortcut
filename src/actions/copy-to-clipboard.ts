46a5298bd5cea03cac916f91a28e490739618a7b
Brandon Jordan <brandonjordan124@gmail.com>
2023-11-25 19:03:54 -0500

    Correct var name

--- src/actions/copy-to-clipboard.ts
+++ src/actions/copy-to-clipboard.ts
@@ -2,7 +2,7 @@ import {renderActionHeader, renderParameters} from "~/render";
 import {actions, actionText} from "~/actions";
 import {renderValue} from "~/value";
 
-interface ShowAlertParameters {
+interface CopyToClipboardParameters {
     WFInput: string | object
     WFLocalOnly: boolean
     WFExpirationDate: string | object
@@ -11,7 +11,7 @@ interface ShowAlertParameters {
 export default {
     title: "Copy",
     icon: "doc_on_doc_fill",
-    render: (container: HTMLElement, params: ShowAlertParameters) => {
+    render: (container: HTMLElement, params: CopyToClipboardParameters) => {
         container.className += ' sp-blue-action';
 
         const action = renderActionHeader(actions['setclipboard'],
