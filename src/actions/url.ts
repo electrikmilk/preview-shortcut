d5cbaf471908305418a41e905b9654d872e57796
Brandon Jordan <brandonjordan124@gmail.com>
2023-11-30 20:40:02 -0500

    Default value for URL action

--- src/actions/url.ts
+++ src/actions/url.ts
@@ -12,10 +12,12 @@ export default {
         container.className += ' sp-blue-action';
 
         let links: HTMLElement[] = [];
-        if (params['WFURLActionURL']) {
+        if (params['WFURLActionURL'].length !== 0) {
             params['WFURLActionURL'].forEach(url => {
                 links.push(renderValue(url, 'URL'));
             });
+        } else {
+            links.push(renderValue(null, 'URL'));
         }
 
         return renderActionHeader(actions['url'], ...links);
