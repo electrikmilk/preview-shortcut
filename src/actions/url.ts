20360d420806df2bb7312290eec3469f66652b84
Brandon Jordan <brandonjordan124@gmail.com>
2024-04-26 19:47:26 -0400

    Fix URL action

--- src/actions/url.ts
+++ src/actions/url.ts
@@ -23,7 +23,7 @@ export default {
                 });
             }
         } else {
-            links.push(renderValue(params.WFURLActionURL.Value))
+            links.push(renderValue(params.WFURLActionURL))
         }
 
         if (!links.length) {
