5bf226b9770d49d467b6fcb19967be844fc3631b
Brandon Jordan <brandonjordan124@gmail.com>
2024-04-26 10:40:56 -0400

    Fix run shortcut action

--- src/actions/run-shortcut.ts
+++ src/actions/run-shortcut.ts
@@ -1,9 +1,10 @@
 import {renderActionHeader, renderParameters} from "~/render";
-import {actions} from "~/actions";
 import {renderValue} from "~/value";
+import {actions} from "~/actions";
 
 interface RunShortcutParameters {
     WFInput: string | object
+    WFWorkflowName: string | Object
     WFWorkflow: string | object
     output: string | object
 }
@@ -12,11 +13,13 @@ export default {
     title: "Run",
     icon: "",
     render: (container: HTMLElement, params: RunShortcutParameters) => {
+        let shortcut = params['WFWorkflowName'];
+
         container.classList.add('sp-shortcut-action');
 
-        const action = renderActionHeader(actions['runworkflow'], renderValue(params['WFInput'], 'Shortcut'));
+        const action = renderActionHeader(actions['runworkflow'], renderValue(shortcut, 'Shortcut'));
         action.appendChild(renderParameters(actions['alert'], {
-            'Input': params['WFInput'],
+            'Input': params.WFInput,
         }));
 
         return action;
