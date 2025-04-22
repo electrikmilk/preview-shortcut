b5d933ebb5785f55f9538d0bb38ddd32e7b0f27e
Brandon Jordan <brandonjordan124@gmail.com>
2025-04-21 22:55:48 -0400

    Fix types in menu actions

--- src/actions/choose-from-menu.ts
+++ src/actions/choose-from-menu.ts
@@ -12,30 +12,25 @@ interface ChooseFromMenuParameters {
     WFMenuItemAttributedTitle: string | object
 }
 
-interface ChooseFromMenuItem {
-    WFItemType: number,
-    WFValue: string | object
-}
-
 export default {
     icon: 'square_list',
     background: Colors.LightBlue,
     render: (container: HTMLElement, params: ChooseFromMenuParameters) => {
         let action;
-        if (params['WFControlFlowMode'] == 0) {
+        if (params.WFControlFlowMode == 0) {
             action = renderActionHeader(actions['choosefrommenu'],
                 actionText('Choose from menu with'),
-                renderValue(params['WFMenuPrompt'] ?? null, 'Prompt')
+                renderValue(params.WFMenuPrompt ?? null, 'Prompt')
             );
 
             const list = document.createElement('div');
             list.className = 'sp-action-list-content sp-unstyled-value';
             const ul = document.createElement('ul');
-            if (params['WFMenuItems'].length !== 0) {
-                for (let item in params['WFMenuItems']) {
+            if (params.WFMenuItems.length !== 0) {
+                for (let item in params.WFMenuItems) {
                     const li = document.createElement('li');
                     // @ts-ignore
-                    const value = renderValue(params['WFMenuItems'][item].WFValue, 'Menu Item');
+                    const value = renderValue(params.WFMenuItems[item].WFValue, 'Menu Item');
                     value.classList.add('sp-unstyled-value');
                     li.appendChild(value);
                     ul.appendChild(li);
@@ -43,8 +38,8 @@ export default {
             }
             const footer = document.createElement('div');
             footer.className = 'sp-action-list-footer';
-            footer.innerText = params["WFMenuItems"].length + ' item';
-            if (params["WFMenuItems"].length !== 1) {
+            footer.innerText = params.WFMenuItems.length + ' item';
+            if (params.WFMenuItems.length !== 1) {
                 footer.innerText += 's';
             }
 
@@ -56,10 +51,10 @@ export default {
         } else {
             const header: HTMLElement[] = [];
 
-            if (params['WFControlFlowMode'] == 2) {
+            if (params.WFControlFlowMode == 2) {
                 header.push(actionText('End Menu'));
-            } else if (params['WFControlFlowMode'] == 1) {
-                const value = renderValue(params['WFMenuItemTitle'] ?? params['WFMenuItemAttributedTitle'] ?? null, 'Item');
+            } else if (params.WFControlFlowMode == 1) {
+                const value = renderValue(params.WFMenuItemTitle ?? params.WFMenuItemAttributedTitle ?? null, 'Item');
                 value.classList.add('sp-unstyled-value');
                 value.setAttribute('style', 'font-weight: bold');
                 header.push(value);
