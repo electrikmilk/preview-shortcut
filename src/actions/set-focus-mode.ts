4f51ef45b96b28c43773f460ef595941be7cc8db
Brandon Jordan <brandonjordan124@gmail.com>
2025-09-04 10:44:36 -0400

    Improve set focus mode action

--- src/actions/set-focus-mode.ts
+++ src/actions/set-focus-mode.ts
@@ -7,12 +7,38 @@ import {Colors} from "~/colors";
 interface DoNotDisturbParameters {
     Operation: string
     Enabled: boolean
+    AssertionType: string
+    Event: object | string
+    Time: object | string
 }
 
 export default {
     background: Colors.Purple,
     icon: 'moon_fill',
     render: (_container: HTMLElement, params: DoNotDisturbParameters) => {
-        return renderActionHeader(actions['dnd.set'], renderValue(params.Operation ?? 'Turn'), actionText("Do Not Disturb"), renderValue(params.Enabled ? 'On' : 'Off', 'Enabled'));
+        let header = [];
+        if (params.Operation !== "Toggle") {
+            renderValue(params.Enabled ? 'On' : 'Off', 'Enabled')
+
+            if (params.Enabled) {
+                header.push(actionText("until"), renderValue(params.AssertionType ?? 'Turned Off'));
+
+                if (params.AssertionType) {
+                    if (params.AssertionType === "Time") {
+                        header.push(renderValue(params.Time));
+                    }
+                    switch (params.AssertionType) {
+                        case "Time":
+                            header.push(renderValue(params.Time));
+                            break;
+                        case "Event Ends":
+                            header.push(renderValue(params.Event));
+                            break;
+                    }
+                }
+            }
+        }
+
+        return renderActionHeader(actions['dnd.set'], renderValue(params.Operation ?? 'Turn'), actionText("Do Not Disturb"), ...header);
     }
 }
